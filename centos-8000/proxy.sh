8b4b60482ca481641df35745703d7cf1c59150e1
zarndearc <75034073+zarndearc@users.noreply.github.com>
2024-05-31 23:42:07 +0530

    done

---
+++ centos-8000/proxy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update system and install required packages
+yum update -y
+yum install nano firewalld wget -y
+
+# Start firewalld service
+systemctl start firewalld
+
+# Download and run Squid Proxy Installer script
+wget https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid3-install.sh
+bash squid3-install.sh
+
+# Change Squid proxy port to 5001
+sed -i 's/3128/8000/g' /etc/squid/squid.conf
+
+# Add firewall rule to allow traffic on port 5001
+firewall-cmd --permanent --zone=public --add-port=5001/tcp
+firewall-cmd --reload
+
+# Restart Squid service
+systemctl restart squid
