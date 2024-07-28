ec5d703bfe4caa524a0f42d6b1a11b1b753ef583
zarndearc <75034073+zarndearc@users.noreply.github.com>
2024-07-28 16:21:51 +0530

    Update sok-find-os.sh

--- ramaya/sok-find-os.sh
+++ ramaya/sok-find-os.sh
@@ -9,6 +9,8 @@
 
 if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 22.04" > /dev/null; then
     echo "ubuntu2204"
+elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 24.04" > /dev/null; then
+    echo "ubuntu2204"
 elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 20.04" > /dev/null; then
     echo "ubuntu2004"
 elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 18.04" > /dev/null; then
