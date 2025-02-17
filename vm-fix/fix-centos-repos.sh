4f5bc83fd3423e390d04a4a0117d4884d9dd0e20
zarndearc <75034073+zarndearc@users.noreply.github.com>
2025-02-17 06:51:43 +0530

    DOne

--- vm-fix/fix-centos-repos.sh
+++ vm-fix/fix-centos-repos.sh
@@ -39,7 +39,55 @@ EOL
 
 echo "AppStream repository file created."
 
-# Clean and update package cache
+# Extras Repo
+cat > /etc/yum.repos.d/CentOS-Stream-Extras.repo <<EOL
+[extras]
+name=CentOS Stream 9 - Extras
+baseurl=https://mirror.stream.centos.org/9-stream/extras/\$basearch/os/
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+EOL
+
+echo "Extras repository file created."
+
+# High Availability Repo
+cat > /etc/yum.repos.d/CentOS-Stream-HA.repo <<EOL
+[ha]
+name=CentOS Stream 9 - HighAvailability
+baseurl=https://mirror.stream.centos.org/9-stream/HighAvailability/\$basearch/os/
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+EOL
+
+echo "High Availability repository file created."
+
+# PowerTools (CRB) Repo
+cat > /etc/yum.repos.d/CentOS-Stream-PowerTools.repo <<EOL
+[crb]
+name=CentOS Stream 9 - CRB (PowerTools)
+baseurl=https://mirror.stream.centos.org/9-stream/CRB/\$basearch/os/
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+EOL
+
+echo "PowerTools (CRB) repository file created."
+
+# NFV Repo
+cat > /etc/yum.repos.d/CentOS-Stream-NFV.repo <<EOL
+[nfv]
+name=CentOS Stream 9 - NFV
+baseurl=https://mirror.stream.centos.org/9-stream/NFV/\$basearch/os/
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+EOL
+
+echo "NFV repository file created."
+
+# Cleaning and updating package manager cache
 echo "Cleaning and updating package manager cache..."
 dnf clean all
 yum clean all
